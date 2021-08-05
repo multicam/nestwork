d1ed8d85bdb3573651d4330ef54074b66aeb239e
multicam <jmgiorgi@gmail.com>
2021-08-05 20:54:37 +1000

    scaffold /7 -- data hooks /5

--- src/routes/listing/[slug].json.js
+++ src/routes/listing/[slug].json.js
@@ -1,8 +1,23 @@
-import {readFileSync,readdirSync} from 'fs'
+import {readFileSync,readdirSync,statSync} from 'fs'
 import {join} from 'path'
 
 const {log} = console
 
+const getAllFiles = function(dirPath, arrayOfFiles) {
+  const files = readdirSync(dirPath)
+
+  arrayOfFiles = arrayOfFiles || []
+
+  files.forEach(function(file) {
+    if (statSync(dirPath + "/" + file).isDirectory()) {
+      arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles)
+    } else {
+      arrayOfFiles.push(join(dirPath, "/", file))
+    }
+  })
+
+  return arrayOfFiles
+}
 
 const getFile = name => {
   log('loading', name, new Date())
@@ -16,7 +31,7 @@ export async function get({ params }) {
       slug: slug,
       // raw: getFile(`${slug}.json`)
       // raw: getFile(`test-something.json`)
-      raw: readdirSync('.')
+      raw: getAllFiles('.')
     }
   }
 }
