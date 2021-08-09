4371f48ad9163805d6bcb3c1070a80eeaaaa4f75
multicam <jmgiorgi@gmail.com>
2021-08-09 20:41:23 +1000

    calculator /1

---
+++ src/components/input-money.svelte
@@ -0,0 +1,21 @@
+<script>
+
+  export let value;
+
+  function int2str(i) {
+    return i ? i.toLocaleString("en-AU") : 0
+  }
+  function str2int(s) {
+    return parseInt(s.replace(/\,/g, ""));
+  }
+
+  let internal = int2str(value);
+
+  function refresh() {
+    value = str2int(internal);
+    internal = int2str(value);
+  }
+
+</script>
+
+<input class='input' type="text" bind:value={ internal } on:input={ refresh }>
--- src/routes/calculator.svelte
+++ src/routes/calculator.svelte
@@ -1,88 +1,155 @@
 <script>
-  export async function load({page, fetch, session, context}) {
-    console.log('--', page,fetch,session,context)
-    return {
-      annual_income: 0.00
+  import MoneyInput from '../components/input-money.svelte'
+  import Toggle from '../components/toggle.svelte'
+
+  const {log} = console, {stringify} = JSON
+  //
+  // 0 – $18,200 | Nil
+  // $18,201 – $45,000 | 19 cents for each $1 over $18,200
+  // $45,001 – $120,000 | $5,092 plus 32.5 cents for each $1 over $45,000
+  // $120,001 – $180,000 | $29,467 plus 37 cents for each $1 over $120,000
+  // $180,001 and over | $51,667 plus 45 cents for each $1 over $180,000
+  //
+  const taxBracket = [
+    {
+      max: 18200,
+      bt: 0,
+      rn: 0
+    },
+    {
+      min: 18201,
+      max: 45000,
+      bt: 0,
+      rn: .19
+    },
+    {
+      min: 45001,
+      max: 120000,
+      bt: 5092,
+      rn: .325
+    },
+    {
+      min: 120001,
+      max: 180000,
+      bt: 29467,
+      rn: .37
+    },
+    {
+      min: 180001,
+      bt: 51667,
+      rn: .45
     }
+  ]
+
+  // -----------------
+  const calculateTaxes = val => {
+    if(!val) return {}
+
+    const last = taxBracket.slice(-1)[0]
+    const first = taxBracket[0]
+
+    if( val < first.max ) {
+      return {
+        bracket: first,
+        amount: val * first.rn
+      }
+    }
+
+    if( val > last.min ) {
+      return {
+        bracket: last,
+        amount: last.bt + ( val - last.min - 1 ) * last.rn
+      }
+    }
+
+    const res = taxBracket.find( i => val <= i.max && val >= i.min )
+    return {
+      bracket: res,
+      amount: res.bt + ( val - res.min - 1 ) * res.rn
+    }
+
   }
+
+  let yearlyIncome
+  $: taxAmount = calculateTaxes(yearlyIncome)
+
+  const formatMoney = val => {
+    if( !val ) return '-'
+    return new Intl.NumberFormat('en-AU', { style: 'currency', currency: 'AUD', maximumFractionDigits: 0, minimumFractionDigits: 0 }).format(val)
+  }
+
+  const formatPercent = val => `${Math.round(val * 100.)}%`
+
+  const propertyData = {
+    sharePrice: 760000
+  }
+
+  const lvrValues = [ .60, .65, .70, .75, .80 ]
+
+  let selectedLvr = lvrValues[0]
+
+  $: loanAmount = propertyData.sharePrice * selectedLvr
+
 </script>
 
-<main class="flex items-center justify-center">
-    <div class="flex flex-col w-full rounded-lg shadow bg-white sm:w-1/2 lg:max-w-xl">
-        <h1
-                class="m-0 py-2 px-4 font-thin text-3xl text-white bg-green-700 rounded-t"
-        >
-            Currency Converter
-        </h1>
-        <div class="flex flex-col items-center justify-center w-full pt-4">
-            <label for="amountToConvert">Amount to convert:</label>
-            <input
-                    id="amountToConvert"
-                    class="block py-2 px-3 leading-6 border border-gray-300 rounded"
-                    name="amountToConvert"
-                    type="number"
-                    min="0"
-                    placeholder="Amount"
-            />
+<main class="calculator-container">
+    <h1>Live Calculator</h1>
+    <section>
+        <h3>Sample Property</h3>
+        <pre>{stringify(propertyData)}</pre>
+    </section>
+    <section>
+        <h3>Annual Income</h3>
+        <div class="input-group">
+            <MoneyInput bind:value={yearlyIncome} />
         </div>
-
-        <div class="flex items-center w-full h-full py-4 text-center">
-            <div class="flex flex-col items-center w-3/5 px-4 sm:w-full">
-                <label for="fromCurrency">Convert From:</label>
-                <input
-                        id="fromCurrency"
-                        class="block w-full py-2 px-3 leading-6 border border-gray-300 rounded"
-                        name="fromCurrency"
-                        maxlength="3"
-                        list="fromCurrencyList"
-                />
-                <datalist id="fromCurrencyList">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                </datalist>
-            </div>
-
-            <button
-                    class="flex items-center justify-center m-0 bg-transparent border-2
-        border-transparent pointer focus:border-gray-300"
-            >
-                <svg
-                        class="w-6 h-6"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 512 512"
-                >
-                    <path
-                            d="M377.941
-            169.941V216H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029
-            239.029c-9.373 9.373-9.373 24.568 0 33.941l86.059 86.059c15.119
-            15.119 40.971 4.411 40.971-16.971V296h243.882v46.059c0 21.382 25.851
-            32.09 40.971 16.971l86.059-86.059c9.373-9.373 9.373-24.568
-            0-33.941l-86.059-86.059c-15.119-15.12-40.971-4.412-40.971 16.97z"
-                    />
-                </svg>
-            </button>
-
-            <div class="flex flex-col items-center w-3/5 px-4 sm:w-full">
-                <label for="toCurrency">Convert To:</label>
-                <input
-                        id="toCurrency"
-                        class="block w-full py-2 px-3 leading-6 border border-gray-300 rounded"
-                        name="toCurrency"
-                        maxlength="3"
-                        list="toCurrencyList"
-                />
-                <datalist id="toCurrencyList">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                </datalist>
-            </div>
-        </div>
-
-        <div class="flex flex-wrap justify-center w-full pb-2">
-            <p>Conversion Result Goes Here.</p>
-        </div>
-
-    </div>
+    </section>
+    <section>
+        <h3>Tax Position</h3>
+        <div class="money">{formatMoney(taxAmount?.amount)}</div>
+        <pre>{stringify(taxAmount)}</pre>
+    </section>
+    <section>
+        <h3>LVR</h3>
+        {#each lvrValues as lvr }
+            <Toggle type='radio' bind:group={selectedLvr} value={lvr} let:checked={checked}>
+                {checked ? '>> ' : '' }{formatPercent(lvr)}
+            </Toggle>
+        {/each}
+        <pre>{stringify(selectedLvr)}</pre>
+    </section>
+    <section>
+        <h3>Share Price</h3>
+        <div class="money"><span>Total</span>{formatMoney(propertyData.sharePrice)}</div>
+        <div class="money"><span>Loan</span>{formatMoney(loanAmount)}</div>
+    </section>
 </main>
+
+<style lang="scss">
+
+    .calculator-container {
+      font-family: var(--font-sans);
+      padding: 0 60px ;
+    }
+
+    section {
+      margin-top: 1em;
+    }
+
+    pre {
+      white-space: pre-wrap;
+      word-break: keep-all;
+      color: rgba(0,0,0,.5)
+    }
+
+    .money {
+      font-size: 1.6em;
+      color: orangered;
+
+      span {
+        color: rgba(0,0,0,.3);
+        font-size: .616em;
+        margin-right: 1em;
+      }
+    }
+</style>
