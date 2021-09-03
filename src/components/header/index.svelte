02323e38b48624b676d8e49577df5040e81c9708
multicam <jmgiorgi@gmail.com>
2021-09-03 14:11:55 +1000

    home /1.8

--- src/components/header/index.svelte
+++ src/components/header/index.svelte
@@ -1,5 +1,7 @@
 <script>
     import Headroom from 'svelte-headroom'
+    import Logo from "../icons/logo.svelte"
+
     const {log} = console
     const pin = e => {
       log('pin', e)
@@ -10,13 +12,14 @@
 </script>
 
 <Headroom duration="200ms" on:pin={pin} on:unpin={unpin}>
-    <header>
-        <h1>Coucou</h1>
+    <header class="px">
+        <Logo/>
     </header>
 </Headroom>
 
 <style>
     header {
         display: flex;
+        height: 5vw;
     }
 </style>
