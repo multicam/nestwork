adc35c92081796177e92ff3f83e0083479d5fde2
multicam <jmgiorgi@gmail.com>
2021-09-11 08:59:07 +1000

    site /1.16

--- src/routes/property/[slug].svelte
+++ src/routes/property/[slug].svelte
@@ -1,4 +1,11 @@
 <script>
     const {stringify} = JSON
+    export async function load({page, fetch, session, context}) {
+      log(page)
+      return {
+        status: 200,
+        error: new Error(`Can't even...`)
+      };
+    }
 </script>
-<pre>{stringify(page.params)}</pre>
+<pre>coucou</pre>
